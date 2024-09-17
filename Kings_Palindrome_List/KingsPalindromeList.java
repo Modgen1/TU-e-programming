99b3dfed116f401b9383c65f4189e715ca250757
Modgen1 <vanyappap@gmail.com>
2024-09-17 15:47:20 +0200

    Code refactoring

--- Kings_Palindrome_List/KingsPalindromeList.java
+++ Kings_Palindrome_List/KingsPalindromeList.java
@@ -7,7 +7,8 @@ import java.util.*;
  * Usage:
  * First line of input is the task that should be performed.
  * Second line of input is the Natural number which indicates the amount of numbers in the list.
- * Third line of input consists of numbers separated by a space which make up the King's palindrome list.
+ * Third line of input consists of numbers separated by a space which make up
+ * the King's palindrome list.
  * 
  * @author Ivan Sergeevich Mishin
  * @ID 2076209
@@ -31,7 +32,8 @@ class KingsPalindromeList {
             numbers[i] = sc.next(); //scanning for all input numbers and filling numbers list
         }
 
-        String answer = switch (taskNumber) { //calling methods accordingly to the input task number and storing answer
+        String answer = switch (taskNumber) {
+            //calling methods accordingly to the input task number and storing answer
             case 1 -> taskOne();
             case 2 -> taskTwo();
             case 3 -> taskThree();
@@ -41,23 +43,30 @@ class KingsPalindromeList {
     }
 
     /**
-     * Method for finding the smallest palindromes greater than or equal to the numbers from the King's list.
+     * Method for finding the smallest palindromes greater than or equal
+     * to the numbers from the King's list.
      * @return result string containing the correct list of palindromes.
      */
     String taskOne() {
-        StringBuilder correctList = new StringBuilder(); //creating modifiable array that will collect the correct numbers
-        for (String original : numbers) { //for-loop going through each number from the King's list
-            String reversed = new StringBuilder(original).reverse().toString(); //reversing the number
+        StringBuilder correctList = new StringBuilder();
+        //creating modifiable array that will collect the correct numbers
+        for (String original : numbers) {
+            //for-loop going through each number from the King's list
+            String reversed = new StringBuilder(original).reverse().toString();
+            //reversing the number
             if (!reversed.equals(original)) {
-                int number = Integer.parseInt(original); //convert the number from the list to int data type
-                while (!reversed.equals(original)) { //while loop until the number becomes a palindrome
+                int number = Integer.parseInt(original);
+                //convert the number from the list to int data type
+                while (!reversed.equals(original)) {
+                    //while loop until the number becomes a palindrome
                     number += 1;
-                    original = Integer.toString(number); //convert the number from int to str
+                    original = Integer.toString(number);
+                    //convert the number from int to str
                     reversed = new StringBuilder(original).reverse().toString();
                 }
             }
-            correctList.append(original).append(" "); //add the number to the array with the correct numbers
-
+            correctList.append(original).append(" ");
+            //add the number to the array with the correct numbers
         }
         return correctList.toString().trim();
     }
@@ -67,11 +76,14 @@ class KingsPalindromeList {
      * @return result string containing the length of longest magic set.
      */
     String taskTwo() {
-        numbers = taskOne().split(" "); //calling first method to convert all numbers to palindromes
-        int maxMagicNumber = 1; //integer value for storing the maximum length of magic set
+        numbers = taskOne().split(" ");
+        //calling first method to convert all numbers to palindromes
+        int maxMagicNumber = 1;
+        //integer value for storing the maximum length of magic set
         for (String number : numbers) {
             int currentMagicNumber = 1;
-            if (number.length() > maxMagicNumber * 2) { //checks whether number is longer than already found magic set
+            if (number.length() > maxMagicNumber * 2) {
+                //checks whether number is longer than already found magic set
                 for (int i = 0; i < (number.length() - 1) / 2; i++) {
                     for (String comparing : numbers) {
                         if (comparing.equals(number.substring(i + 1, number.length() - (i + 1)))) {
@@ -94,8 +106,10 @@ class KingsPalindromeList {
      * @return string containing all elements of the longest magic set in ascending order.
      */
     String taskThree() {
-        numbers = taskOne().split(" "); //calling first method to convert all numbers to palindromes
-        ArrayList<String> longestMagicSet = new ArrayList<>(); //arraylist containing the longest magic set
+        numbers = taskOne().split(" ");
+        //calling first method to convert all numbers to palindromes
+        ArrayList<String> longestMagicSet = new ArrayList<>();
+        //arraylist containing the longest magic set
         for (String number : numbers) {
             ArrayList<String> currentMagicSet = new ArrayList<>();
             currentMagicSet.add(number);
@@ -108,18 +122,22 @@ class KingsPalindromeList {
                         }
                     }
                 }
-                // the third task requires additional check for the case when there are multiple longest magic sets
+                // the third task requires additional check for the case
+                // when there are multiple longest magic sets
                 if (currentMagicSet.size() > longestMagicSet.size()) {
                     longestMagicSet = currentMagicSet;
                 } else if (currentMagicSet.size() == longestMagicSet.size()
-                        && Integer.parseInt(currentMagicSet.get(0)) > Integer.parseInt(longestMagicSet.get(0))) {
+                        && Integer.parseInt(currentMagicSet.get(0))
+                        > Integer.parseInt(longestMagicSet.get(0))) {
                     longestMagicSet = currentMagicSet;
                 }
             }
         }
-        Collections.reverse(longestMagicSet); // reversing the arraylist to make elements in ascending order
+        Collections.reverse(longestMagicSet);
+        // reversing the arraylist to make elements in ascending order
         StringBuilder correctList = new StringBuilder();
-        for (String number : longestMagicSet) { // this cycle is to build the answer string from elements of arraylist
+        for (String number : longestMagicSet) {
+            // this cycle is to build the answer string from elements of arraylist
             correctList.append(number).append(" ");
         }
         return correctList.toString().trim();
