dc3d5bed126472f687fd861db952b31e410ffd5e
westn1k3 <gamolin.nv@gmail.com>
2024-09-30 20:32:37 +0200

    comments

--- Animal_Keeper/AnimalKeeper.java
+++ Animal_Keeper/AnimalKeeper.java
@@ -2,8 +2,25 @@ import java.util.*;
 
 /**
  * Purpose:
+ * The program helps to keep track of the animal situation in the Zoo.
  *
  * Usage:
+ * There are 5 different commands that the user can input.
+ * 1. 0 t "name" h: adds an animal of type t with a name "name" to the home with number h.
+ * 2. 1 "name" h: moves the animal with a name "name" to a home with number h.
+ * 3. 2 "name": remove the animal with name "name" from the Zoo.
+ * 4. 3 f x: buys food of type f the amount x.
+ * 5. 4 f x h: feeds food of type f the amount x to home h.
+ *
+ * There are several restrictions listed below which should not be violated.
+ * There are 10 cages number 0 to 9 and 5 enclosures numbered 10 to 14.
+ * Each animal should have a unique name.
+ * There is a single storage for each type of food which has a maximum capacity of 100.
+ * The food has to be bought before it can be fed to the animals.
+ * Herbivores cannot be kept with carnivores and omnivores.
+ * Carnivores and omnivores do not like to live with other carnivore and omnivore species.
+ * Tigers, Leopards and Bears are solitary creatures that do not want to live in a group.
+ * A cage can fit up to two animals. An open enclosure up to six.
  *
  * @author Ivan Sergeevich Mishin
  * @ID 2076209
@@ -15,7 +32,7 @@ public class AnimalKeeper {
 
     Scanner sc = new Scanner(System.in);
 
-    MyZoo zoo = new MyZoo(); // object of MyZoo class from second file MyZoo.java
+    MyZoo zoo = new MyZoo(); // object of MyZoo class from the second file MyZoo.java
 
     int command; // command number that is inputted every iteration
 
