585ac6305caa6545e393c31d521f49d6c95c98a1
Modgen1 <vanyappap@gmail.com>
2024-09-30 14:23:47 +0200

    Comments for main file

--- Animal_Keeper/AnimalKeeper.java
+++ Animal_Keeper/AnimalKeeper.java
@@ -15,10 +15,11 @@ public class AnimalKeeper {
 
     Scanner sc = new Scanner(System.in);
 
-    MyZoo zoo = new MyZoo();
+    MyZoo zoo = new MyZoo(); // object of MyZoo class from second file MyZoo.java
 
-    int command;
+    int command; // command number that is inputted every iteration
 
+    // different types of arguments that can be used for certain commands
     int animalType;
     String animalName;
     int homeNumber;
@@ -26,40 +27,42 @@ public class AnimalKeeper {
     int foodAmount;
 
     /**
-     *
+     * Main method that runs in the loop until faces invalid command number, then stops.
+     * For each command it gets required inputs and passes them to relative methods in MyZoo class.
      */
     void run() {
         while (true) {
             command = sc.nextInt();
             switch (command) {
-                case 0:
+                case 0: // command to add new animal
                     animalType = sc.nextInt();
                     animalName = sc.next();
                     homeNumber = sc.nextInt();
                     zoo.addAnimal(animalType, animalName, homeNumber);
                     break;
-                case 1:
+                case 1: // command to move an existing animal to other home
                     animalName = sc.next();
                     homeNumber = sc.nextInt();
                     zoo.moveAnimal(animalName, homeNumber);
                     break;
-                case 2:
+                case 2: // command to remove an existing animal
                     animalName = sc.next();
                     zoo.removeAnimal(animalName);
                     break;
-                case 3:
+                case 3: // command to buy certain amount of certain type of food
                     foodType = sc.nextInt();
                     foodAmount = sc.nextInt();
                     zoo.buyFood(foodType, foodAmount);
                     break;
-                case 4:
+                case 4: // command to spend certain amount of certain type of food to feed animals.
                     foodType = sc.nextInt();
                     foodAmount = sc.nextInt();
                     homeNumber = sc.nextInt();
                     zoo.feedAnimal(foodType, foodAmount, homeNumber);
                     break;
-                default:
+                default: // if command number is invalid, program stops without throwing exception
                     System.out.println();
+                    // it is required by the task that program must end with the new line
                     return;
             }
         }
